b69787747c694316a52347338fd449dc77550406
ybenali <youssef.benali@digimind.com>
2023-03-19 16:54:51 +0000

    style: setup custom theme for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,58 @@
-/** @type {import('tailwindcss').Config} */
+const withOpacity = (variableName) => {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}),${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+};
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html'],
   darkMode: ['class', '[data-mode="dark"]'],
   mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: withOpacity('--color-primary'),
+        secondary: withOpacity('--color-secondary'),
+      },
+      backgroundColor: {
+        fill: {
+          light: '#ffffff',
+          DEFAULT: '#ffffff',
+          dark: '#121212',
+        },
+        button: {
+          muted: withOpacity('--color-button-muted'),
+          accent: '#ffffff',
+        },
+        // input: { DEFAULT: 'var(--color-input)' },
+        skin: {},
+      },
+
+      textColor: {
+        base: {
+          light: '#3d3d3d',
+          DEFAULT: '#3d3d3d',
+          dark: '#b8b8b8',
+        },
+        muted: {},
+        inverted: {
+          light: '#000000',
+          DEFAULT: '#000000',
+          dark: '#ffffff',
+        },
+      },
+
+      borderColor: {
+        button: {
+          muted: withOpacity('--color-button-muted-border'),
+        },
+        // input: {
+        //   DEFAULT: withOpacity('--color-input-border'),
+        // },
+      },
+    },
+  },
 };
